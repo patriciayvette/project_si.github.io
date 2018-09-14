687461c4eba9e4de6afc323f393a7531d2a98b81
patriciayvette <patricia.clarisa@gmail.com>
2018-09-14 08:51:58 +0700

    a

--- application/views/footer-home.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer -->
-<footer>
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3" style="color:black;padding-top:30px;">
-    Sistem Informasi - 2018
-  </div>
-  <!-- Copyright -->
-</footer>
-<!-- Footer -->
--- application/views/greet.php
+++
@@ -1,5 +0,0 @@
-<div class="clearfix">
-		<div >
-			Selamat datang <strong><?php echo $this->session->userdata('admin_nama'); ?></strong> 
-		</div>			
-      </div>
